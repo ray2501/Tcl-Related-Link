b56fec9a10de24c103fdbbf6a03605447f15f7c4
Danilo Chang <ray2501@gmail.com>
2019-08-21 16:59:47 +0800

    Update

--- script/rename.tcl
+++ script/rename.tcl
@@ -6,6 +6,8 @@
 #
 
 package require jpeg
+package require Img
+package require imgtools
 
 puts "##########   Start   ##########"
 if {$argc >= 1} {
@@ -22,9 +24,26 @@ foreach filename [glob *.jpg] {
    set today2 $today
    append today2 "_"   
 
-   regsub -all {\mIMG} $filename $today newFileName   
+   regsub -all {\mIMG} $filename $today newFileName
    regsub -all {\mCAM0} $filename $today2 newFileName
+   regsub -all {\mIMAG} $filename $today2 newFileName
    file rename $filename $newFileName
+
+   # For large image, resize to 1024x768 or 768x1024
+   image create photo untiled -format JPEG -file $newFileName
+   image create photo destImg
+
+   set height [image height untiled]
+   set width [image width untiled]
+   if {$width == 4032} {
+       ::imgtools::scale untiled 1024x768 destImg
+       destImg write $newFileName -format JPEG
+   } elseif {$height == 4032} {
+       ::imgtools::scale untiled 768x1024 destImg
+       destImg write $newFileName -format JPEG
+   }
+
+   image delete untiled destImg
 }
 
 puts "##########    End    ##########"
